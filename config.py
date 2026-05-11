34efe9c8b29ac4599381fc4c1922e199748cb899
Arjun Dubey <dubeyarjun864@gmail.com>
2026-05-11 14:13:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,9 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+# Sirf ye IDs hi bot use kar payengi
 ADMIN_IDS = [int(i.strip()) for i in os.getenv("ADMIN_IDS", "").split(",") if i.strip()]
+
 CHANNEL_ID = "@ArjunBotz"
 CHANNEL_LINK = "https://t.me/ArjunBotz"
 ADMIN_LINK = "https://t.me/Arjun_Dubey"
